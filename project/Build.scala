83dca5faa9bb0cb0a1706d9ca653de87ede90adf
ephe-meral <j.appel.online@gmail.com>
2013-10-20 21:32:25 +0200

    Added default maps + serialization

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,8 @@ object ProjectBuild extends Build {
     lazy val root = Project(
         id = "server", 
         base = file(".")
-    ) dependsOn(protocol)
+    ) dependsOn(protocol, jsonMacros)
 
     lazy val protocol = RootProject(uri("https://github.com/entice/protocol.git#milestone3"))
+    lazy val jsonMacros = RootProject(uri("https://github.com/ephe-meral/akmacros-json.git#fix-play-2.2-SNAPSHOT"))
 }
--- src/main/scala/entice/server/world/PathingMap.scala
+++ src/main/scala/entice/server/world/PathingMap.scala
@@ -4,27 +4,58 @@
 
 package entice.server.world
 
-import entice.protocol._
+import entice.protocol.Coord2D
+import play.api.libs.json._
+import info.akshaal.json.jsonmacro._
+import scala.io._
+import scala.util._
 
 
 /**
  * Consists of the walkable trapezoids and their connections.
  */
-case class PathingMap          (trapezoids: Map[Int, Trapezoid],
-                                connections: Set[HorizontalConnection])
+class PathingMap(
+    val trapezoids: Set[Trapezoid],
+    val connections: Set[HorizontalConnection])
+
+object PathingMap {
+    // deserialization
+    import entice.protocol.Utils._    
+    implicit def horizontalBorderFactory            = factory[HorizontalBorder]             ('fromJson)
+    implicit def simpleHorizontalConnectionFactory  = factory[SimpleHorizontalConnection]   ('fromJson)
+    implicit def verticalBorderFactory              = factory[VerticalBorder]               ('fromJson)
+    implicit def trapezoidFactory                   = factory[Trapezoid]                    ('fromJson)
+    implicit def simplePathingMapFactory            = factory[SimplePathingMap]             ('fromJson)
+
+    /**
+     * Try to apply from file (can fail with None)
+     */
+    def apply(file: String): Option[PathingMap] = {
+        Try(Json.fromJson[SimplePathingMap](Json.parse(Source.fromFile(file).mkString.trim)).get) match {
+            case pmap: Success[_] => Some(PathingMap.sophisticate(pmap.get))
+            case pmap: Failure[_] => None
+        }
+    }
+
+    /**
+     * Creates associations from the IDs used in the simple versions of connections
+     */
+    private def sophisticate(simple: SimplePathingMap) = {
+        val traps: Map[Int, Trapezoid] = (for (t <- simple.trapezoids) yield (t.id -> t)).toMap
+        val conns: Set[HorizontalConnection] =
+            (for (c <- simple.connections) yield 
+                HorizontalConnection(c.west, c.east, c.y, traps(c.north), traps(c.south)))
+            .toSet
+        new PathingMap(traps.values.toSet, conns)
+    }
+}
 
 
 /**
  * Simplified non-associative version, for serialization purposes.
  */
-case class SimplePathingMap    (trapezoids: List[Trapezoid],
-                                connections: List[HorizontalConnection]) {
-    def sophisticate = {
-        PathingMap(
-            trapezoids  = (for (t <- trapezoids) yield (t.id -> t)).toMap,
-            connections = connections.toSet)
-    }
-}
+case class SimplePathingMap    (trapezoids: Array[Trapezoid],
+                                connections: Array[SimpleHorizontalConnection])
 
 
 /**
@@ -60,8 +91,13 @@ sealed trait HorizontalEdge {
 }
 case class HorizontalBorder    (west: Float, east: Float, y: Float) extends HorizontalEdge
 case class HorizontalConnection(west: Float, east: Float, y: Float,
-                                northernTrapezoid: Int,
-                                southernTrapezoid: Int)             extends HorizontalEdge
+                                northern: Trapezoid,
+                                southern: Trapezoid)                extends HorizontalEdge
+case class SimpleHorizontalConnection
+                                (west: Float, east: Float, y: Float,
+                                id: Int, // TODO: deprecated
+                                north: Int,
+                                south: Int)                         extends HorizontalEdge
 
 
 /**
