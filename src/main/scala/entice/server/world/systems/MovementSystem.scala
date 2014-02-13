8f43c9fd143933aa7fbb20fdd2cf12a5297d7ba8
ephe-meral <j.appel.online@gmail.com>
2014-02-13 17:53:59 +0100

    changed movement system due to protocol changes

--- src/main/scala/entice/server/world/systems/MovementSystem.scala
+++ src/main/scala/entice/server/world/systems/MovementSystem.scala
@@ -30,12 +30,15 @@ class MovementSystem extends System[Position :: Movement :: HNil] with Actor wit
 
         entities(world)
             .filter  { e => 
+                val curPos  = e[Position].pos
+                val curGoal = e[Movement].goal
                 (e[Movement].moveState != NotMoving &&
-                 e[Movement].dir       != Coord2D(0, 0)) 
+                 curGoal - curPos      != Coord2D(0, 0)) 
             }
             .foreach { e =>
-                val curPos = e[Position].pos
-                val curDir = e[Movement].dir
+                val curPos  = e[Position].pos
+                val curGoal = e[Movement].goal
+                val curDir  = curGoal - curPos
 
                 // TODO: add movementspeed here, add trapezoid
                 val nextPos = (curPos + (curDir.unit * 288)) * timeDiff
@@ -43,7 +46,15 @@ class MovementSystem extends System[Position :: Movement :: HNil] with Actor wit
                 // check out the next position and set it
                 world.pmap.nextValidPosition(curPos, nextPos) match {
                     case Some(pos) if (pos != curPos) => 
+                        // set the new position
                         e.set[Position](e[Position].copy(pos = pos))
+                        // get a new goal
+                        world.pmap.farthestPosition(pos, curDir) match {
+                            case Some(goal) if (goal != pos) =>
+                                e.set[Movement](e[Movement].copy(goal = goal))
+                            case _ =>
+                                e.set[Movement](e[Movement].copy(state = NotMoving.toString))
+                        }
                     case _ => 
                         e.set[Movement](e[Movement].copy(state = NotMoving.toString))
                 }
